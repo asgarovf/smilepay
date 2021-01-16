7c9b23ec7a1e833367512864f66776019c1fb1b8
asgarovf <fasgarov2002@gmail.com>
2021-01-16 23:21:37 +0400

    update

--- js/script.js
+++ js/script.js
@@ -25,6 +25,9 @@ $(document).ready(function () {
 
   $(document).scroll(function () {
     scroll_pos = $(this).scrollTop();
+    var windowHeight = jQuery(window).height();
+    var topOfWindow = $(window).scrollTop();
+
     if (scroll_pos > 20) {
       $("#navbar-main").addClass("nav-shadow");
     } else {
@@ -33,25 +36,25 @@ $(document).ready(function () {
 
     var scrolltop = $("#phone_1").offset();
     var scrolltop2 = $("#phone_2").offset();
-    if (scroll_pos > scrolltop.top - 800) {
+    if (topOfWindow + windowHeight - 200 > scrolltop.top) {
       $("#phone1-image-div").addClass("phone1-image-animation");
       $("#phone1-image-div-sm").addClass("phone1-image-animation");
       $("#phone1-text-div").addClass("phone1-text-div-animation");
     }
 
-    if (scroll_pos > scrolltop.top) {
+    if (topOfWindow + windowHeight - 750 > scrolltop.top) {
       $("#div2-images").addClass("div2-images-animation");
       $("#div2-images-sm").addClass("div2-images-animation");
       $("#div2-text").addClass("div2-text-animation");
     }
 
-    if (scroll_pos > scrolltop2.top - 500) {
+    if (topOfWindow + windowHeight - 200 > scrolltop2.top) {
       $("#phone2-image-div").addClass("phone1-image-animation");
       $("#phone2-image-div-sm").addClass("phone1-image-animation");
       $("#phone2-text-div").addClass("phone1-text-div-animation");
     }
 
-    if (scroll_pos > scrolltop2.top - 100) {
+    if (topOfWindow + windowHeight - 750 > scrolltop2.top) {
       $("#div3-images").addClass("div2-images-animation");
       $("#div3-images-sm").addClass("div2-images-animation");
       $("#div3-text").addClass("div2-text-animation");
